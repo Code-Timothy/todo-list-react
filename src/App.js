367a49c9e5c23ca0c1be6c9e04dd64c8b0c0b534
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-03-17 23:56:18 +0100

    Form component added with styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-
+import Form from "./Form";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <main className="container">
+      <header>
+        <h1>Lista zadań</h1>
+      </header>
+      <section className="section">
+        <header className="section__header">
+          <h2 className="section__title">Dodaj nowe zadanie</h2>
+        </header>
+        <div className="section__body">
+          <Form />
+        </div>
+      </section>
+      <section className="section">
+        <header className="section__header">
+          <h2 className="section__title">Lista zadań</h2>
+          <div className="buttons"></div>
+        </header>
+        <div className="section__body">
+          <ul className="tasksList"></ul>
+        </div>
+      </section>
+    </main>
   );
 }
 

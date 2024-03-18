e594ec869670054d915ba7e4cc4be68369989115
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-03-18 20:39:15 +0100

    Section component added with styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Form from "./Form";
 import Buttons from "./Buttons";
 import Tasks from "./Tasks";
+import Section from "./Section";
 
 const tasks = [
   { content: "Learn some new English words!", done: false },
@@ -15,23 +16,15 @@ function App() {
       <header>
         <h1>Lista zadań</h1>
       </header>
-      <section className="section">
-        <header className="section__header">
-          <h2 className="section__title">Dodaj nowe zadanie</h2>
-        </header>
-        <div className="section__body">
-          <Form />
-        </div>
-      </section>
-      <section className="section">
-        <header className="section__header">
-          <h2 className="section__title">Lista zadań</h2>
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
-        </header>
-        <div className="section__body">
-          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
-        </div>
-      </section>
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+      <Section
+        title="Lista zadań"
+        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+      />
     </main>
   );
 }

d959cf0810573cd61864ba0394eed3d6ad126137
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-03-18 21:29:17 +0100

    code formatted

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ const hideDoneTasks = false;
 function App() {
   return (
     <Container>
-      <Header title="Lista zadań" />
+      <Header
+        title="Lista zadań"
+      />
       <Section
         title="Dodaj nowe zadanie"
         body={<Form />}

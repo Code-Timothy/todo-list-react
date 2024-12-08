3cf75f7ad589efcdabf956acabdc0a567eecfb71
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-08 22:11:05 +0100

    add Form component with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./index.css";
+import Form from "./Form";
 
 function App() {
   return (
-    <div >
-
-    </div>
+    <main className="container">
+      <header className="header">
+        <h1>Lista zadań</h1>
+      </header>
+      <section className="section">
+        <header className="section__header">
+          <h2 className="section__title">Dodaj nowe zadanie</h2>
+        </header>
+        <div className="section__content">
+          <Form />
+        </div>
+      </section>
+      <section className="section">
+        <header className="section__header">
+          <h2 className="section__title">Lista zadań</h2>
+          <div className="buttons"></div>
+        </header>
+        <div className="section__content">
+          <ul className="tasksList"></ul>
+        </div>
+      </section>
+    </main>
   );
 }
 

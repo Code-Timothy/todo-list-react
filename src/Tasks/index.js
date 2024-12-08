7d3325d75c110443f7bb52d2b04c0d8cfc2405c7
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-08 22:58:42 +0100

    rename wrong class name in Tasks component

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="tasksList">
         {tasks.map((task) => (
-            <li className={`tasksList-item ${task.done && hideDoneTasks ? "tasksList__item--hidden" : ""}`}>
+            <li className={`tasksList__item ${task.done && hideDoneTasks ? "tasksList__item--hidden" : ""}`}>
                 <button className="tasksList__button">{task.done ? "✔" : ""}</button>
                 <span className={task.done ? "tasksList__item--done" : ""}>{task.content}</span>
                 <button className="tasksList__button tasksList__button--remove">🗑</button>

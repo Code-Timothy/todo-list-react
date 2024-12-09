61726001e5e62ddb05265701f92702f18c424588
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-09 17:38:03 +0100

    Change attribute autofocus to autoFocus

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Form = () => (
     <form className="form">
         <input className="form__input" type="text" placeholder="Co jest do zrobienia?" required
-            autofocus />
+            autoFocus />
         <button className="form__button">Dodaj zadanie</button>
     </form>
 );

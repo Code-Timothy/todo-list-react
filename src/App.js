41d02b5e76a311f1f264d0a5aa93801ba3d3fb72
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-10 19:42:11 +0100

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -32,6 +32,10 @@ function App() {
     }));
   };
 
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -42,7 +46,7 @@ function App() {
       <Section
         title="Lista zadań"
         extraHeaderContent={
-          <Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />
+          <Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} setAllDone={setAllDone} />
         }
         body={
           <Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} toggleTaskDone={toggleTaskDone} />
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     tasks.length > 0 &&
     <div className="buttons">
         <button
@@ -11,6 +11,7 @@ const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
         </button>
         <button
             className="buttons__button"
+            onClick={setAllDone}
             disabled={tasks.every(({ done }) => done)}
         >
             Ukończ wszystkie

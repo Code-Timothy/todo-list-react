b390b109fe5ee68515d6e152196e87f5e976eb1b
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-13 22:00:06 +0100

    add styled-components to Section component

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,15 +1,15 @@
-import "./style.css";
+import { StyledSection, Header, Title, Content } from "./styled";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <section className="section">
-        <header className="section__header">
-            <h2 className="section__title">{title}</h2>
+    <StyledSection>
+        <Header>
+            <Title>{title}</Title>
             {extraHeaderContent}
-        </header>
-        <div className="section__content">
+        </Header>
+        <Content>
             {body}
-        </div>
-    </section>
+        </Content>
+    </StyledSection>
 );
 
 export default Section;
--- src/Section/styled.js
+++ src/Section/styled.js
@@ -1,9 +1,11 @@
-.section {
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
     background-color: white;
     margin: 5px;
-}
+`;
 
-.section__header {
+export const Header = styled.header`
     display: grid;
     grid-template-columns: auto auto;
     gap: 20px;
@@ -11,18 +13,16 @@
     border-bottom: 1px solid #ddd;
     justify-content: space-between;
     align-items: center;
-}
 
-@media (max-width:767px) {
-    .section__header {
+    @media (max-width: 767px){
         grid-template-columns: 1fr;
     }
-}
+`;
 
-.section__title {
+export const Title = styled.h2`
     margin: 0;
-}
+`;
 
-.section__content {
+export const Content = styled.div`
     padding: 20px;
-}
+`;

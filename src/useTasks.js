8cd8148292a106796b79be8d276445462f9fd443
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-13 23:28:23 +0100

    add useTasks custom hook for task management

---
+++ src/useTasks.js
@@ -0,0 +1,73 @@
+import { useLocalStorageState } from "./useLocalStorageState";
+
+export const useTasks = () => {
+    const [tasks, setTasks] = useLocalStorageState("tasks", [])
+
+    const removeTask = (id) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
+    };
+
+    const toggleTaskDone = (id) => {
+        setTasks(tasks => tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, done: !task.done };
+            }
+
+            return task;
+        }));
+    };
+
+    const editTask = (id) => {
+        setTasks(tasks => tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, isEdit: !task.isEdit }
+            }
+
+            return task;
+        }));
+    };
+
+    const saveEditedTask = (id, newContent) => {
+        if (!newContent.trim()) {
+            return;
+        }
+
+        setTasks(tasks => tasks.map((task) => {
+            if (task.id === id) {
+                return { ...task, content: newContent, isEdit: false };
+            }
+
+            return task;
+        }));
+    };
+
+    const setAllDone = () => {
+        setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
+    };
+
+    const sortTasks = () => {
+        setTasks(tasks => [...tasks].sort((a, b) => a.content.localeCompare(b.content)));
+    };
+
+    const addNewTask = (newTaskContent) => {
+        setTasks(tasks => ([
+            ...tasks,
+            {
+                id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+                content: newTaskContent,
+                done: false,
+            },
+        ]));
+    };
+
+    return {
+        tasks,
+        removeTask,
+        toggleTaskDone,
+        editTask,
+        saveEditedTask,
+        setAllDone,
+        sortTasks,
+        addNewTask,
+    };
+};

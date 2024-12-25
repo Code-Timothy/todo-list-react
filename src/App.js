06a0d7ee9d3a15d1c3362dffa5998f7529aeee75
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-26 00:22:26 +0100

    set up routing with HashRouter and add routes for tasks and author

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import Tasks from "./features/tasks/index";
+import Author from "./features/author/index";
+import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
 
-const App = () => <Tasks />
+const App = () => (
+    <HashRouter>
+        <Link to="/zadania">Zadania</Link>
+        <Link to="/autor">O autorze</Link>
+        <Switch>
+            <Route path="/zadania">
+                <Tasks />
+            </Route>
+            <Route path="/autor">
+                <Author />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
+    </HashRouter>
+)
 
 export default App;
---
+++ src/features/author/index.js
@@ -0,0 +1,5 @@
+const Author = () => (
+    <>Test</>
+)
+
+export default Author;

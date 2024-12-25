0f3bdf6b242b0a7ad9fce90f2f6472c0e2927bab
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-25 23:31:59 +0100

    add selectLoading selector

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -65,6 +65,7 @@ export const {
 export const selectTasksState = state => state.tasks;
 
 export const selectHideDone = state => selectTasksState(state).hideDone;
+export const selectLoading = state => selectTasksState(state).loading;
 export const selectTasks = state => selectTasksState(state).tasks;
 export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);

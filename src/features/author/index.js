8e033221bcc58099f50abdfeee96b1838b38816a
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-26 00:36:19 +0100

    add content to Author component

--- src/features/author/index.js
+++ src/features/author/index.js
@@ -1,5 +1,17 @@
+import Section from "../../common/Section";
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+
 const Author = () => (
-    <>Test</>
-)
+    <Container>
+        <Header title="O autorze" />
+        <Section
+            title="Tymoteusz Wójcik"
+            body={
+                <p>About me</p>
+            }
+        />
+    </Container>
+);
 
 export default Author;

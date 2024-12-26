0bcc9109f79fae597379205a7ee11167ee43370b
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-26 21:38:52 +0100

    add selectTasksByQuery selector

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -74,4 +74,14 @@ export const selectTaskIsEdit = state => selectTasks(state).find(({ isEdit }) =>
 export const getTaskById = (state, taskId) =>
     selectTasks(state).find(({ id }) => id === taskId);
 
+export const selectTasksByQuery = (state, query) => {
+    const tasks = selectTasks(state);
+
+    if (!query || query.trim() === "") {
+        return tasks;
+    }
+
+    return tasks.filter(({ content }) => content.toUpperCase().includes(query.trim().toUpperCase()));
+};
+
 export const tasksReducer = tasksSlice.reducer;

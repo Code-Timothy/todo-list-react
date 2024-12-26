4691b4b0e9c70e5bf8305ee92355ada769ff48a0
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-26 22:33:13 +0100

    add styled-components to Navigation component

---
+++ src/common/Navigation/styled.js
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledNavigation = styled.nav`
+    background-color: ${({ theme }) => theme.colors.primaryColor};
+`;
+
+export const StyledNavList = styled.ul`
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    margin: 0;
+    padding: 10px;
+    list-style: none;
+`;
+
+export const StyledNavItem = styled.li`
+    padding: 10px;
+`;
+
+export const StyledNavLink = styled(NavLink)`
+    &.active{
+        font-weight: bold;
+    }
+
+    color: white;
+    text-decoration: none;
+`;

371ba75147c77cafc898c7dd7bc968062df5dc32
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-27 21:31:25 +0100

    add new section to TaskPage and implement comment functionality

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Section from "../../../common/Section";
 import Container from "../../../common/Container";
 import Header from "../../../common/Header";
-import { getTaskById } from "../tasksSlice";
+import { addCommentToTask, getTaskById } from "../tasksSlice";
 
 const TaskPage = () => {
     const { id } = useParams();
     const task = useSelector(state => getTaskById(state, id));
+
+    const [comment, setComment] = useState("");
+    const dispatch = useDispatch();
+
+    const handleAddComment = () => {
+        if (comment.trim() !== "") {
+            dispatch(addCommentToTask({ taskId: id, comment }));
+            setComment("");
+        }
+    };
 
     return (
         <Container>
@@ -17,6 +28,26 @@ const TaskPage = () => {
                 body={
                     <>
                         <strong>Ukończono: </strong> {task ? (task.done ? "Tak" : "Nie") : ""}
+                    </>
+                }
+            />
+            <Section
+                title="Komentarze do zadania"
+                body={
+                    <>
+                        <ul>
+                            {task.comments.map(comment => (
+                                <li key={comment.id}>
+                                    {comment.content}
+                                </li>
+                            ))}
+                        </ul>
+                        <textarea
+                            placeholder="Dodaj komentarz..."
+                            value={comment}
+                            onChange={({ target }) => setComment(target.value)}
+                        />
+                        <button onClick={handleAddComment}>Dodaj komentarz</button>
                     </>
                 }
             />
